43677e1c2742c8db744dcb40802183da530a1577
Reza Qadimi-رضا قدیمی <rezaqadimi.ir@gmail.com>
2022-07-15 19:18:03 +0430

    Add ValidationHelper at SeedWork, And Dtat PersianDate and PersianDateTime at Dtat library.

--- Dtat/Utility.cs
+++ Dtat/Utility.cs
@@ -5,5 +5,34 @@
 		static Utility()
 		{
 		}
+
+		public static System.DateTime Now
+		{
+			get
+			{
+				var currentCulture =
+					System.Threading.Thread.CurrentThread.CurrentCulture;
+
+				var currentUICulture =
+					System.Threading.Thread.CurrentThread.CurrentUICulture;
+
+				var englishCulture =
+					new System.Globalization.CultureInfo(name: "en-US");
+
+				System.Threading.Thread.CurrentThread.CurrentCulture = englishCulture;
+				System.Threading.Thread.CurrentThread.CurrentUICulture = englishCulture;
+
+				var result =
+					System.DateTime.Now;
+
+				//var result =
+				//	System.DateTime.Now.AddMinutes(value: 210);
+
+				System.Threading.Thread.CurrentThread.CurrentCulture = currentCulture;
+				System.Threading.Thread.CurrentThread.CurrentUICulture = currentUICulture;
+
+				return result;
+			}
+		}
 	}
 }
