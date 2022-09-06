c2e501ac579e4f79e60971663e6d09c271359f9e
Mr. Dariush Tasdighi <DariushT@GMail.com>
2022-09-06 14:48:48 +0430

    Adding StateMachine

---
+++ Dtat.StateMachine.Abstractions/ITransition.cs
@@ -0,0 +1,19 @@
+﻿namespace Dtat.StateMachine.Abstractions
+{
+	public interface ITransition<T>
+	{
+		T Id { get; }
+
+		T ToStateId { get; }
+
+		T FromStateId { get; }
+
+		int Code { get; }
+
+		bool IsActive { get; }
+
+		string ActionTitle { get; }
+
+		string Description { get; }
+	}
+}
